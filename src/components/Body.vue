fb6a650bb4d0dd09976fa1d467bb214f19424203
amitlzkpa <amit.lzkpa@gmail.com>
2020-08-09 04:28:23 -0500

    remove bidding enable/disable buttons

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,11 +1,6 @@
 <template>
   <div>    
     <button @click="refresh">Refresh</button>
-    <button v-if="linkedBoardId === null" @click="enableBidding">Enable Bidding</button>
-    <span v-else>
-      {{ linkedBoardId }}
-      <button @click="deleteBidding">Delete Bidding</button>
-    </span>
     <p v-for="row in rows" :key="row.name">
       {{ row.name }}
     </p>
@@ -15,14 +10,12 @@
 <script>
 
 let ctx;
-let key_linkedBidBoard = "test3";
 
 export default {
   data () {
     return {
       user: null,
       currBoardData: null,
-      linkedBoardId: null,
       rows: []
     };
   },
@@ -46,55 +39,6 @@ export default {
       let res = await this.monday.api(queryStr);
       this.currBoardData = res.data.boards[0];
       this.rows = this.currBoardData.items;
-      
-      res = await this.monday.storage.instance.getItem(key_linkedBidBoard);
-      this.linkedBoardId = res.data.value;
-    },
-    async enableBidding() {
-      while(!ctx) await this.wait(200);
-
-      let mutStr;
-      let res;
-
-      mutStr = `mutation { create_board (board_name: "${this.currBoardData.name}[Bids]", board_kind: private) { id } }`;
-      res = await this.monday.api(mutStr);
-
-      this.linkedBoardId = res.data.create_board.id;
-
-      for(let r of this.rows) {
-        mutStr = `mutation { create_column (board_id: ${this.linkedBoardId}, title: "${r.name}", column_type: long_text) { id } }`;
-        res = await this.monday.api(mutStr);
-      }
-
-      await this.monday.storage.instance.setItem(key_linkedBidBoard, this.linkedBoardId);
-      
-      // this request fails for some unknown reason; but work is done on the server
-      try {
-        let extURL = `${this.BIDSERVER_URL}/api/tender/monday?tenderBoardId=${this.currBoardData.id}&bidsBoardId=${this.linkedBoardId}&userId=${this.user.id}&accId=${this.user.account.id}`;
-        await this.$api.get(extURL);
-      } catch(excp) {
-        console.log(excp);
-      }
-
-      await this.monday.execute("notice", { 
-        message: "Enabled bidding for this request. Jump over to the bidding board to invite bidders.",
-        type: "success",
-        timeout: 10000,
-      });
-
-    },
-    async deleteBidding() {
-      let mutStr;
-      let res;
-
-      mutStr = `mutation { archive_board (board_id: ${this.linkedBoardId}) { id name } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      res = await this.monday.storage.instance.deleteItem(key_linkedBidBoard);
-      console.log(res.data);
-
-      this.linkedBoardId = null;
     }
   }
 }
